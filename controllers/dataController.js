5b03776eefdee1849a4bb0c6d0c2d8efd356bd08
Vernon Mensah <vernonmensah@gmail.com>
2018-03-14 12:04:58 -0400

    accidental delete

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -86,7 +86,7 @@ module.exports = {
     'deleteWordSets': function() {
         return WordSetModel.find()
             .then((data) => {
-                data = ;
+                console.log(data); 
                 return data.save();
             })
             .catch((err) => {
