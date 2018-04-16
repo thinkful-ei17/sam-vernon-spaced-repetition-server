e9dda524295cdd5124e27eb9fb5477405722c42c
Sam Gould <sjg326@gmail.com>
2018-04-16 15:14:27 -0700

    made necessary changes to run locally and integrated necessary minimal cors

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ module.exports = {
   JWT_SECRET: process.env.JWT_SECRET || 'hello',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '7d',
   PORT: process.env.PORT || 8080,
-  DATABASE_URL: process.env.DATABASE_URL || 'mongodb://localhost/thinkful-backend',
+  // DATABASE_URL: process.env.DATABASE_URL || 'mongodb://localhost/thinkful-backend',
+  DATABASE_URL: 'mongodb://test:test@ds133776.mlab.com:33776/satutor',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'mongodb://localhost/thinkful-backend-test'
 };
